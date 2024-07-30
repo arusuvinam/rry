98a17f1d47ccc185efa733c30b8e15ab062e32c0
unknown <arusuvinam@gmail.com>
2024-07-30 11:17:14 +0530

    mmm

---
+++ src/org/yy/Enum.java
@@ -0,0 +1,18 @@
+package org.yy;
+
+public class Enum {
+	
+	enum month{
+		jan,feb,mar,april,may,june,july
+	}
+
+	public static void main(String[] args) {
+		
+	    month[] values = month.values();
+		for (month mon : va) {
+			System.out.println(mon);
+			
+		}
+		
+	}
+}
